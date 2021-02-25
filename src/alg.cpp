cc11e83b8e00fbb6ba4da1ef116005d10f351ab9
romanakimkin <78901819+romanakimkin@users.noreply.github.com>
2021-02-25 12:20:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,10 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    while (a>0 && b>0)
+        if (a > b)
+            a%=b;
+        else if(a<b)
+            b%=a;
+        return a+b
 }

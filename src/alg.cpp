c208e557fb1b76ed04b081990a1a197302b39d60
romanakimkin <78901819+romanakimkin@users.noreply.github.com>
2021-02-25 13:42:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,21 @@
 // Copyright 2021 NNTU-CS
-
-int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+#include <iostream>
+using namespace std;
+    void gcd (int a, int b)
+    {
+    while(a > 0 && b > 0)
+    {
+        if(a > b)
+            a %= b;
+        else
+            b %= a;
+    }        
+    cout << a+b << endl;
+    }
+int main()
+{
+    int a,b,c;
+    cin>>a>>b;
+    gcd(a,b);
+    return 0;
 }

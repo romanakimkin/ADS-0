5365aa2d4505ad3355a0139e7385736e1d075a71
romanakimkin <78901819+romanakimkin@users.noreply.github.com>
2021-02-25 13:13:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,25 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
+    #include <iostream>
+using namespace std;
+    void gcd (int a, int b)
+    {
+    while(a > 0 && b > 0)
  
+        if(a > b)
+            a %= b;
+ 
+        else
+            b %= a;
+    cout << a+b << endl;
+    }
+int main()
+{
+    int a,b,c;
+    cin>>a>>b;
+    gcd(a,b);
+    return 0;
+}
+
 }

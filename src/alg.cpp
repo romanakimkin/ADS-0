85622275c1a65c6f1bd042e76b4beb5569df2767
romanakimkin <78901819+romanakimkin@users.noreply.github.com>
2021-02-25 12:12:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,5 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
- cin >> a >> b;
-    while (a>0 && b>0)
-        if (a > b)
-            a%=b;
-        else if(a<b)
-            b%=a;
-       return a+b;
-
+ 
 }

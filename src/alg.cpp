619e8c477dddef82e26dd7d5b43d47497d9c3ce6
romanakimkin <78901819+romanakimkin@users.noreply.github.com>
2021-02-25 14:15:29 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,7 @@
-// Copyright 2021 NNTU-CS
 #include "alg.h"
 int gcd(int a, int b) {
-    while(a > 0 && b > 0)
-    {
-        if(a > b)
+    while ((a > 0) && (b > 0)){
+        if (a > b)
             a %= b;
         else
             b %= a;
